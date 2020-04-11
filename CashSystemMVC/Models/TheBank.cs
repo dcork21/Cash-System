23a22fd927e6a19323bc90bbb298b931e32f5dcc
Dom Cork <dominicnaish-cork@Doms-MacBook-Pro.local>
2020-04-11 11:03:53 +0100

    implement models

---
+++ CashSystemMVC/Models/TheBank.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CashSystemMVC.Models
+{
+    public class The_Bank
+    {
+        public int BankId { get; set; }
+        public string SortCode { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
---
+++ CashSystemMVC/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace CashSystemMVC.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public int IdentityId { get; set; }
+        public int AccountId { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string SortCode { get; set; }
+    
+    }
+}
