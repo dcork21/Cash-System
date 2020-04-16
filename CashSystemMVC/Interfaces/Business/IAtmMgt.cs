cd5bff576d5a28a4b329183bd7d971c70806e179
Gareth Perry <garethnperry@gmail.com>
2020-04-16 09:56:54 +0100

    scaffold Atm and Bank

--- CashSystemMVC/Interfaces/Business/IAtmMgt.cs
+++ CashSystemMVC/Interfaces/Business/IAtmMgt.cs
@@ -1,4 +1,5 @@
 ﻿using CashSystemMVC.Models;
+using NotImplementedException = System.NotImplementedException;
 
 namespace CashSystemMVC.Interfaces.Business
 {
@@ -8,5 +9,41 @@ namespace CashSystemMVC.Interfaces.Business
         Atm GetAtm();
         Atm UpdateAtm();
         bool DeleteAtm();
+    }
+
+
+    public class AtmMgt : IAtmMgt
+    {
+        // The database
+        private readonly DataContext _data;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data">The database context dependency injected via StartUp</param>
+        public AtmMgt(DataContext data)
+        {
+            _data = data;
+        }
+
+        public Atm CreateAtm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Atm GetAtm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Atm UpdateAtm()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAtm()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- CashSystemMVC/Interfaces/Business/IBankMgt.cs
+++ CashSystemMVC/Interfaces/Business/IBankMgt.cs
@@ -1,4 +1,5 @@
-﻿using CashSystemMVC.Models;
+﻿using System;
+using CashSystemMVC.Models;
 
 namespace CashSystemMVC.Interfaces.Business
 {
@@ -8,5 +9,40 @@ namespace CashSystemMVC.Interfaces.Business
         Bank GetBank();
         Bank UpdateBank();
         bool DeleteBank();
-    }
+
+
+        public class BankMgt : IBankMgt
+        {
+            // The database
+            private readonly DataContext _data;
+
+            /// <summary>
+            /// 
+            /// </summary>
+            /// <param name="data">The database context dependency injected via StartUp</param>
+            public BankMgt(DataContext data)
+            {
+                _data = data;
+            }
+
+            public Bank CreateBank()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Bank GetBank()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Bank UpdateBank()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool DeleteBank()
+            {
+                throw new NotImplementedException();
+            }
+        }
 }
